364f9506c5e4ee951ee6a52bdc7cd6c5a8050e66
JustVeres <darthveres@gmail.com>
2026-02-07 23:18:25 +0300

    переписан тест test_ingredient.py

---
+++ tests/test_ingredient.py
@@ -0,0 +1,28 @@
+import pytest
+from praktikum.ingredient import Ingredient
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
+class TestIngredient:
+
+    @pytest.mark.parametrize(
+        "type_ing, name, price",
+        [
+            (INGREDIENT_TYPE_SAUCE, "hot sauce", 100.0),
+            (INGREDIENT_TYPE_FILLING, "sausage", 300.0)
+        ]
+    )
+    def test_ingredient_create(self, type_ing, name, price):
+        """Проверка создания модели ингредиента"""
+        ing = Ingredient(type_ing, name, price)
+
+        assert isinstance(ing.get_type(), str) == True
+        assert isinstance(ing.get_name(), str) == True
+        assert isinstance(ing.get_price(), float) == True
+
+        assert ing.get_type().strip() != ""
+        assert ing.get_name().strip() != ""
+        assert ing.get_price() > 0
+
+        assert ing.get_type() == type_ing
+        assert ing.get_name() == name
+        assert ing.get_price() == price
